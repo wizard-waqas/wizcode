ce363e6c3498b68d82fd3625f307a542cc38ec43
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-17 02:12:59 -0400

    spacing

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({Component, pageProps}) {
     const userData = useUserData()
 
     return (
-        <UserContext.Provider value={userData}>
+        <UserContext.Provider value={ userData }>
             <Navbar/>
             <Component {...pageProps} />
             <Toaster/>
