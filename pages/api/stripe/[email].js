7ed99cbd18f6be8db0a6c3a2d8bb775cb140aeb5
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-26 00:21:18 -0400

    handle stripe requests using nodejs from /api route

---
+++ pages/api/stripe/[email].js
@@ -0,0 +1,24 @@
+import Stripe from "stripe";
+
+export default async function handler(req, res) {
+    const {email} = req.query  // get user email from route
+
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2020-08-27",
+    })
+
+    const customer = await stripe.customers.list({
+        email: email
+    }).then(customers => {
+        // return the first customer from the returned data
+        console.log(customers)
+        return customers.data[0]
+    })
+
+    const invoices = await stripe.invoices.list({
+        customer: customer.id,
+        limit: 3,
+    }).then(invoices => invoices.data)  // return a list of invoices for the user
+
+    res.status(200).json({invoices})
+}
