1744661a03050d14b2c97370214343486e9dbc26
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-30 18:55:45 -0400

    catch possible error

--- pages/api/twilio/[email].js
+++ pages/api/twilio/[email].js
@@ -6,12 +6,13 @@ export default async function handler(req, res) {
     const {email} = req.query  // get user email from route
 
     client.messages
-      .create({
-         body: `Trial requested from ${email}`,
-         from: '+17629003136',
-         to: '+17326684229'
-       })
-      .then(message => console.log(message));
+        .create({
+            body: `Trial requested from ${email}`,
+            from: '+17629003136',
+            to: '+17326684229'
+        })
+        .then(message => console.log(message))
+        .catch(error => console.log(error));
 
     res.status(200).json(email)
 }
