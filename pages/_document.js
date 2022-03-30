c76c71eef9d8bab8c41f7546420f1cff6d7378f4
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-30 17:21:33 -0400

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <link rel="icon" type="image/x-icon" href="/logo.png"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Open+Sans&display=swap"
