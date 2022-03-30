6a31275802463c43e24b9b39ebf57702681e1ca4
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-30 02:37:38 -0400

    formatting

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,6 +1,5 @@
 /* React JS Template using functions */
 import React, {useContext, useEffect} from "react"
-import Link from "next/link";
 import {toast} from "react-hot-toast";
 import {auth, googleAuthProvider} from "../lib/firebase";
 import {UserContext} from "../lib/context";
@@ -14,7 +13,7 @@ export default function NavigationBar() {
         <>
             <Navbar className={"w-full p-6"} fixed={"top"} expand={"md"}
                     bg={"dark"} variant={"dark"}>
-                <Container className={"flex justify-around"}>
+                <Container className={"flex justify-around transition-all"}>
                     <img className={"w-16 cursor-pointer"} src="/logo.png" alt="logo"/>
                     <NavbarToggle aria-controls={"responsive-navbar-nav"}/>
                     <Navbar.Collapse className={"w-full"} id={"responsive-nav-nav"}>
@@ -29,9 +28,13 @@ export default function NavigationBar() {
                                           href={"/lessons/"}>
                                     LESSONS
                                 </Nav.Link>
+                                {/*<Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}*/}
+                                {/*          href={"https://www.etsy.com/listing/1114882742/wizcode-t-shirt-coding-unisex-men-and?click_key=b05d86769133b63325820913eca68044149e5746%3A1114882742&click_sum=fbaef5fe&rec_type=ss&ref=landingpage_similar_listing_top-1"}>*/}
+                                {/*    MERCH*/}
+                                {/*</Nav.Link>*/}
                                 <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
-                                          href={"https://www.etsy.com/listing/1114882742/wizcode-t-shirt-coding-unisex-men-and?click_key=b05d86769133b63325820913eca68044149e5746%3A1114882742&click_sum=fbaef5fe&rec_type=ss&ref=landingpage_similar_listing_top-1"}>
-                                    MERCH
+                                          href={"/invoices/"}>
+                                    INVOICES
                                 </Nav.Link>
                             </ul>
 
