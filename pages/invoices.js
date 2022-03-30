7a6423a332da182596b09fe0275feeb8c7077d79
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-30 02:39:58 -0400

    restyled invoice component with mobile first approach

--- pages/invoices.js
+++ pages/invoices.js
@@ -4,22 +4,21 @@ import {UserContext} from "../lib/context";
 
 export default function InvoicesPage() {
     const {user} = useContext(UserContext);
-    const [invoices, setinvoices] = useState([])
+    const [invoices, setInvoices] = useState([])
 
     useEffect(() => {
         async function fetchData() {
             const response = await fetch(`/api/stripe/${user.email}`)
-            const data = await response.json()
-            setinvoices(data.invoices)
+            return await response.json()
         }
 
         if (user !== null) {
-            fetchData().then(value => console.log(value))
+            fetchData().then(data => setInvoices(data.invoices))
         }
     }, [user])
 
     return (
-        <div className={"flex"}>
+        <div className={"flex flex-col"}>
             {invoices ?
                 invoices.map(invoice => (
                     <Invoice key={invoice.number} invoice={invoice}/>
@@ -33,14 +32,22 @@ export default function InvoicesPage() {
 
 function Invoice({invoice}) {
     return (
-        <div className={"flex bg-blue rounded-xl p-4 m-4 w-4/5"}>
-            <img className={"w-28 mr-4"} src={"/icons/lesson.png"} alt={"lesson icon"}/>
-            <div className={"relative w-full"}>
-                <h1 className={"absolute top-0 right-4"}>{invoice.number}</h1>
-                <p>Amount Due: {getAmountDue(invoice.amount_due)}</p>
-                <span>Paid: <input type={"checkbox"} checked={invoice.paid} readOnly/></span>
-                <a className={"absolute bg-darkgold p-2 rounded-lg bottom-0 right-4"} href={invoice.hosted_invoice_url}>Pay Here &gt;</a>
+        <div className={"flex flex-col bg-blue drop-shadow-xl rounded-xl p-3 my-4 w-full"}>
+            <div className={"flex"}>
+                <img className={"w-16 mr-4"} src={"/icons/lesson.png"} alt={"lesson icon"}/>
+                <div className={"relative w-full"}>
+                    <h1 className={""}>Coding lesson</h1>
+                    <h1 className={"absolute top-0 right-4"}>{invoice.number}</h1>
+                    <p>Amount Due: {getAmountDue(invoice.amount_due)}</p>
+
+                </div>
             </div>
+            {invoice.paid ?
+                <a className={"bg-darkblue p-2 rounded-lg bottom-0 right-4 text-center"}>Paid</a>
+                :
+                <a className={"bg-darkgold p-2 rounded-lg bottom-0 right-4 text-center"}
+                   href={invoice.hosted_invoice_url}>Pay Now</a>
+            }
         </div>
 
     )
