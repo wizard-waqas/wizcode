9207e602eedf7caade66b65dd0c98d98b4cc363c
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-30 17:21:23 -0400

    added logic for submitting an email for a trial

--- components/SplashSection.js
+++ components/SplashSection.js
@@ -1,8 +1,60 @@
 /* React JS Template using functions */
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Tilt from 'react-parallax-tilt';
+import {toast} from "react-hot-toast";
 
 export default function SplashSection() {
+    const [email, setEmail] = useState("")
+    const [hasSubmitted, setHasSubmitted] = useState(false)
+
+    useEffect(() => {
+        const savedEmail = localStorage.getItem("email")
+
+        if (savedEmail){
+            setEmail(savedEmail)
+            setHasSubmitted(true)
+        }
+    }, [])
+
+    async function fetchData() {
+        const response = await fetch(`/api/twilio/${email}`)
+    }
+
+    /**
+     * check if email is valid or not (syntactically)
+     *
+     * @returns {boolean}
+     */
+    function isValidEmail() {
+        const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+        return regex.test(email);
+    }
+
+    /**
+     * handle email input change
+     *
+     * @param e
+     */
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    /**
+     * when user submits their email, make request to twilio
+     * @param event
+     */
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        if (isValidEmail()) {
+            await fetchData()
+            toast.success("Success")
+            localStorage.setItem("email", email)
+            setHasSubmitted(true);
+        } else {
+            toast.error("Invalid email")
+        }
+    }
+
     return (
         <div className={"w-full mt-8"}>
             <div className={"w-full flex"}>
@@ -28,16 +80,24 @@ export default function SplashSection() {
                 </Tilt>
             </div>
 
-            <form className={"mt-16"}>
+            {hasSubmitted ?
+                <div className={"text-3xl text-center mt-16"}>
+                    <span className={"font-fredoka"}>Thank you! </span>
+                    We will be in touch soon to
+                    <span className={"text-gold font-fredoka"}> schedule an appointment</span>.
+                </div>
+                :
+                <div className={"mt-16"}>
                 <h3 className={"text-xl mb-4 lg:text-md"}>Sign up for a free 1-on-1 coding lesson today</h3>
-                <div className={"drop-shadow-lg"}>
+                <form className={"drop-shadow-lg"} onSubmit={handleSubmit}>
                     <input
                         className={"bg-white rounded-tl-lg rounded-bl-lg px-2 py-2 w-4/5 text-darkgrey text-sm md:w-1/2"}
-                        placeholder={"Email *"}/>
-                    <input className={"w-1/5 bg-darkgold rounded-tr-lg rounded-br-lg text-sm px-2 py-2"} type={"submit"}
-                           value={"Submit"}/>
+                        placeholder={"Email *"} onChange={handleChange}/>
+                    <input className={"w-1/5 bg-darkgold rounded-tr-lg rounded-br-lg text-sm px-2 py-2"} type={"button"}
+                           value={"Submit"} onClick={handleSubmit}/>
+                </form>
                 </div>
-            </form>
+            }
         </div>
     )
 }
---
+++ pages/api/twilio/[email].js
@@ -0,0 +1,17 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+export default async function handler(req, res) {
+    const {email} = req.query  // get user email from route
+
+    client.messages
+      .create({
+         body: `Trial requested from ${email}`,
+         from: '+17629003136',
+         to: '+17326684229'
+       })
+      .then(message => console.log(message));
+
+    res.status(200).json(email)
+}
