d048eed8eeca847e65c5b232e038cdea984c4a7d
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-03 02:01:48 -0400

    seperated the SignUpForTrial logic from the main SplashSection

--- components/SplashSection.js
+++ components/SplashSection.js
@@ -9,6 +9,38 @@ import {Bounce} from "react-awesome-reveal";
  *      title, descriptions, coding editor art
  */
 export default function SplashSection() {
+
+    return (
+        <div className={"w-full mt-8"}>
+            <div className={"w-full flex"}>
+                <div className={"w-1/2 mt-4"}>
+                    <h1 className={"text-6xl mb-2"}>
+                        <span className={"font-fredoka text-blue"}>Wiz</span>
+                        <span className={"font-fredoka text-gold"}>Code</span>
+                    </h1>
+
+                    <h2 className={"text-xl"}>
+                        Learn to teach yourself
+                        <span className={"font-fredoka text-gold"}> how to code</span>.
+                    </h2>
+
+                    <h4>
+                        DESCRIPTION GOES HERE
+                    </h4>
+                </div>
+
+                <Tilt className={"w-1/2 self-center mt-20"}>
+                    <img className={"m-4 drop-shadow-lg rounded-lg"} data-tilt src={"editorart.png"}
+                         alt={"coding editor art"}/>
+                </Tilt>
+            </div>
+
+            <SignUpForTrial/>
+        </div>
+    )
+}
+
+const SignUpForTrial = () => {
     const [email, setEmail] = useState("")  // email entered in input box
     const [hasSubmitted, setHasSubmitted] = useState(false)  // has the user already submitted their email
 
@@ -39,7 +71,17 @@ export default function SplashSection() {
      */
     function isValidEmail() {
         const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-        return regex.test(email);
+        const isValid = regex.test(email);
+
+        if (isValid){
+            toast.success("Success")
+            setHasSubmitted(true);
+            localStorage.setItem("email", email)
+        } else {
+            toast.error("Invalid email")
+        }
+
+        return isValid
     }
 
     /**
@@ -59,61 +101,35 @@ export default function SplashSection() {
         event.preventDefault()
         if (isValidEmail()) {
             await sendText()
-            toast.success("Success")
-            localStorage.setItem("email", email)
-            setHasSubmitted(true);
-        } else {
-            toast.error("Invalid email")
         }
     }
 
+    // if user has previously submitted an email then show a "Thank you" message
+    if (hasSubmitted) {
+        return (
+            <Bounce>
+                <div className={"text-3xl text-center mt-16"}>
+                    <span className={"font-fredoka"}>Thank you! </span>
+                    We will be in touch soon to
+                    <span className={"text-gold font-fredoka"}> schedule an appointment</span>.
+
+                </div>
+            </Bounce>
+        )
+    }
+
+    // otherwise (normally), show an input box asking for user email
     return (
-        <div className={"w-full mt-8"}>
-            <div className={"w-full flex"}>
-                <div className={"w-1/2 mt-4"}>
-                    <h1 className={"text-6xl mb-2"}>
-                        <span className={"font-fredoka text-blue"}>Wiz</span>
-                        <span className={"font-fredoka text-gold"}>Code</span>
-                    </h1>
-
-                    <h2 className={"text-xl"}>
-                        Learn to teach yourself
-                        <span className={"font-fredoka text-gold"}> how to code</span>.
-                    </h2>
-
-                    <h4>
-                        DESCRIPTION GOES HERE
-                    </h4>
-                </div>
-
-                <Tilt className={"w-1/2 self-center mt-20"}>
-                    <img className={"m-4 drop-shadow-lg rounded-lg"} data-tilt src={"editorart.png"}
-                         alt={"coding editor art"}/>
-                </Tilt>
-            </div>
-
-            {hasSubmitted ?
-                <Bounce>
-                    <div className={"text-3xl text-center mt-16"}>
-                        <span className={"font-fredoka"}>Thank you! </span>
-                        We will be in touch soon to
-                        <span className={"text-gold font-fredoka"}> schedule an appointment</span>.
-
-                    </div>
-                </Bounce>
-                :
-                <div className={"mt-16"}>
-                    <h3 className={"text-xl mb-4 lg:text-md"}>Sign up for a free 1-on-1 coding lesson today</h3>
-                    <form className={"drop-shadow-lg"} onSubmit={handleSubmit}>
-                        <input
-                            className={"bg-white rounded-tl-lg rounded-bl-lg px-2 py-2 w-4/5 text-darkgrey text-sm md:w-1/2"}
-                            placeholder={"Email *"} onChange={handleChange}/>
-                        <input className={"w-1/5 bg-darkgold rounded-tr-lg rounded-br-lg text-sm px-2 py-2"}
-                               type={"button"}
-                               value={"Submit"} onClick={handleSubmit}/>
-                    </form>
-                </div>
-            }
+        <div className={"mt-16"}>
+            <h3 className={"text-xl mb-4 lg:text-md"}>Sign up for a free 1-on-1 coding lesson today</h3>
+            <form className={"drop-shadow-lg"} onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    className={"bg-white rounded-tl-lg rounded-bl-lg px-2 py-2 w-4/5 text-darkgrey text-sm md:w-1/2"}
+                    placeholder={"Email *"} onChange={(e) => handleChange(e)}/>
+                <input className={"w-1/5 bg-darkgold rounded-tr-lg rounded-br-lg text-sm px-2 py-2"}
+                       type={"button"}
+                       value={"Submit"} onClick={(e) => handleSubmit(e)}/>
+            </form>
         </div>
     )
 }
