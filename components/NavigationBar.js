99a45e5d8f047ce2d86cdd38657f400bca758aac
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-05-06 15:02:28 -0400

    added Link tag and removed lessons tab

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -5,6 +5,7 @@ import {auth, googleAuthProvider} from "../lib/firebase";
 import {UserContext} from "../lib/context";
 import {Navbar, Nav, Container} from "react-bootstrap";
 import NavbarToggle from "react-bootstrap/NavbarToggle";
+import Link from "next/link";
 
 /**
  * Navigation bar shown at top of screen
@@ -18,7 +19,11 @@ export default function NavigationBar() {
             <Navbar className={"w-full p-6"} fixed={"top"} expand={"md"}
                     bg={"dark"} variant={"dark"}>
                 <Container className={"flex justify-around transition-all"}>
-                    <img className={"w-16 cursor-pointer drop-shadow-outline"} src="/logo.png" alt="logo"/>
+                    <Link href={"/"}>
+                        <a>
+                            <img className={"w-16 cursor-pointer drop-shadow-outline"} src="/logo.png" alt="logo"/>
+                        </a>
+                    </Link>
                     <NavbarToggle aria-controls={"responsive-navbar-nav"}/>
                     <Navbar.Collapse className={"w-full"} id={"responsive-nav-nav"}>
                         <Nav className={"w-full justify-around"}>
@@ -28,27 +33,25 @@ export default function NavigationBar() {
                                           href={"/"}>
                                     HOME
                                 </Nav.Link>
-                                <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
-                                          href={"/lessons/"}>
-                                    LESSONS
-                                </Nav.Link>
                                 {/*<Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}*/}
-                                {/*          href={"https://www.etsy.com/listing/1114882742/wizcode-t-shirt-coding-unisex-men-and?click_key=b05d86769133b63325820913eca68044149e5746%3A1114882742&click_sum=fbaef5fe&rec_type=ss&ref=landingpage_similar_listing_top-1"}>*/}
-                                {/*    MERCH*/}
+                                {/*          href={"/lessons/"}>*/}
+                                {/*    LESSONS*/}
                                 {/*</Nav.Link>*/}
+                                <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
+                                          href={"https://www.etsy.com/listing/1114882742/wizcode-t-shirt-coding-unisex-men-and?click_key=b05d86769133b63325820913eca68044149e5746%3A1114882742&click_sum=fbaef5fe&rec_type=ss&ref=landingpage_similar_listing_top-1"}>
+                                    MERCH
+                                </Nav.Link>
                                 <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
                                           href={"/invoices/"}>
                                     INVOICES
                                 </Nav.Link>
                             </ul>
 
-                            <a href="#">
-                                {user ?
-                                    <SignOutButton/>
-                                    :
-                                    <SignInButton/>
-                                }
-                            </a>
+                            {user ?
+                                <SignOutButton/>
+                                :
+                                <SignInButton/>
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -85,5 +88,5 @@ const SignOutButton = () => {
         toast.success("Successfully signed out")
     }
 
-    return <button className={"px-4 py-2 bg-blue rounded-lg"} onClick={signOutwithGoogle}>Sign Out</button>
+    return <button className={"px-4 py-2 w-40 bg-blue rounded-lg"} onClick={signOutwithGoogle}>Sign Out</button>
 }
