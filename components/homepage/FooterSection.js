85245271778ba3cd973bbd72686c14daa713cd77
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-05-07 17:47:41 -0400

    created footer section for important stuff

---
+++ components/homepage/FooterSection.js
@@ -0,0 +1,10 @@
+/* React JS Template using functions */
+import React, {useState, useEffect} from "react"
+
+export default function FooterSection() {
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    )
+}
