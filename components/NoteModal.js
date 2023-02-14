9b01fe207bff17808c8b36cce626fa6526fe2eb4
waqasabbasi <waqaspathan00@gmail.com>
2023-02-14 16:26:46 -0500

    show notes data dynamically from firebase

--- components/NoteModal.js
+++ components/NoteModal.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react"
 import {RiCloseCircleLine} from "react-icons/ri";
 
-export default function NoteModal({handleClick, handleCloseClick}) {
+export default function NoteModal({note, setModalOn}) {
+
+    const handleCloseClick = () => {
+        setModalOn(false)
+    }
 
     return (
         <div className="fixed z-30 inset-0 overflow-y-auto">
@@ -13,27 +17,29 @@ export default function NoteModal({handleClick, handleCloseClick}) {
                         </button>
                     </div>
                     <div className="text-lg font-bold text-gray-800 mb-4 w-full">
-                        <h3 className={"text-gold text-4xl"}>Intro to JavaScript</h3>
-                        <h5 className={"text-grey-300 text-2xl"}>March 21, 2022</h5>
-                        <iframe className="w-full h-96 my-4" src="https://replit.com/@waqasp/intro-to-javascript-DOM?lite=true"/>
+                        <h3 className={"text-gold text-4xl"}>{note.title}</h3>
+                        <h5 className={"text-grey-300 text-2xl"}>{note.date}</h5>
+                        <iframe className="w-full h-96 my-4" src={`${note.replitLink}?lite=true`}/>
 
                         <h4 className={"text-gold text-3xl"}>What I Learned</h4>
                         <ul className={"list-disc [&>*]:ml-12 [&>*]:my-1"}>
-                            <li><p>Grab HTML objects from the DOM</p></li>
-                            <li><p>Grab the text entered from an input box</p></li>
-                            <li><p>Call a function when a button is clicked</p></li>
+                            {note.topicsLearned.map((item) => {
+                                return (
+                                    <li><p>{item}</p></li>
+                                )
+                            })}
                         </ul>
 
                         <h4 className={"text-gold text-3xl"}>Homework</h4>
                         <ul className={"list-disc [&>*]:ml-8 [&>*]:my-2"}>
-                            <li className={"flex items-center"}>
-                                <button className={"w-6 h-6 mr-4 border-2 bg-grey-100 rounded-md"}></button>
-                                <p>Finish the JavaScript DOM tutorial</p>
-                            </li>
-                            <li className={"flex items-center"}>
-                                <button className={"w-6 h-6 mr-4 border-2 bg-grey-100 rounded-md"}></button>
-                                <p>Center items on screen</p>
-                            </li>
+                            {note.homework.map((item) => {
+                                return (
+                                    <li className={"flex items-center"}>
+                                        <button className={"w-6 h-6 mr-4 border-2 bg-grey-100 rounded-md"}></button>
+                                        <p>{item.task}</p>
+                                    </li>
+                                )
+                            })}
                         </ul>
                     </div>
                 </div>
--- pages/notes.js
+++ pages/notes.js
@@ -1,55 +1,96 @@
-import React, {useState} from "react"
+import React, {useContext, useEffect, useState} from "react"
 import NoteModal from "../components/NoteModal";
+import {UserContext} from "../lib/context";
+import {firestore} from "../lib/firebase";
 
 export default function NotesPage() {
+    const {user} = useContext(UserContext);
     const [modalOn, setModalOn] = useState(false)
-    const [choice, setChoice] = useState(false)
+    const [notes, setNotes] = useState([])
 
-    const handleCloseClick = () => {
-        setChoice(false)
-        setModalOn(false)
-    }
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                let doc = await firestore.collection(`users/${user.uid}/notes`).get();
+                const notes = doc.docs.map((doc) => doc.data());
+                console.log(notes)
+                return notes
+            } catch (e) {
+                console.log(e)
+            }
+        }
 
-    const handleClick = () => {
-        setModalOn(true)
+        // only fetch data if there is a user logged in
+        if (user !== null) {
+            fetchData().then(data => setNotes(data))
+        }
+    }, [user])
+
+    if (!notes) {
+        return <div>Loading...</div>
     }
 
     return (
         <div className={"flex flex-wrap"}>
+            {notes && notes.map((note) => {
+                return (
+                    <ViewNoteCard note={note} modalOn={modalOn} setModalOn={setModalOn}/>
+                )
+            })}
 
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
-            <ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick} handleCloseClick={handleCloseClick} />
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
+            {/*<ViewNoteCard title={"Codinewavdsavdg Lesson"} date={getTodaysDate()} handleClick={handleClick}*/}
+            {/*              handleCloseClick={handleCloseClick}/>*/}
 
-            <div className={"grid w-full p-2 justify-items-center"}>
-                {modalOn ? <NoteModal handleCloseClick={handleCloseClick} /> : null}
-            </div>
         </div>
     )
 }
 
-const ViewNoteCard = ({title, date, handleClick}) => {
-
-
+const ViewNoteCard = ({note, modalOn, setModalOn}) => {
     return (
-        <div className={"flex flex-col items-center bg-blue m-3 p-2 rounded-xl"}>
-            <h2 className={""}>{title}</h2>
-            <p className={""}>{date}</p>
-            <img className={"my-2 w-32"} src={"/img/notes/online-lesson.png"}/>
-            <button onClick={handleClick} className={"bg-gold text-black my-2 p-1 w-2/3 rounded-lg"}>VIEW</button>
-        </div>
+        <>
+            <div className={"flex flex-col items-center bg-blue m-3 p-2 rounded-xl"}>
+                <h2 className={""}>{note.title}</h2>
+                <p className={""}>{note.date}</p>
+                <img className={"my-2 w-32"} src={"/img/notes/online-lesson.png"}/>
+                <button onClick={(q) => {
+                    setModalOn(true)
+                }} className={"bg-gold text-black my-2 p-1 w-2/3 rounded-lg"}>VIEW
+                </button>
+            </div>
+
+            <div className={"grid w-full p-2 justify-items-center"}>
+                {modalOn ? <NoteModal note={note} setModalOn={setModalOn}/> : null}
+            </div>
+        </>
     )
 }
 
 // function to get todays date in this format: Month Day, Year
 function getTodaysDate() {
     const date = new Date();
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString('default', {month: 'long'});
     const day = date.getDate();
     const year = date.getFullYear();
     return month + " " + day + ", " + year;
     // example output: "September 13, 2021"
+}
+
+// function to get todays date in this format: mmddyy, only the last 2 numbers of year, padded with 0 if needed
+function getTodaysDateShort() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear().toString().substr(-2);
+    return month + day + year;
+    // example output: "091321"
 }
